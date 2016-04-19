752db642edb0b08f45c6cc7c0b8f6a218dac3078
John <475934874@qq.com>
2016-04-19 11:37:22 +0800

    MainActivity->添加按钮

--- app/src/main/java/com/org/hl/john/framentdemo1/MainActivity.java
+++ app/src/main/java/com/org/hl/john/framentdemo1/MainActivity.java
@@ -2,6 +2,10 @@ package com.org.hl.john.framentdemo1;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by John on 2016/4/19.
@@ -11,5 +15,28 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Button button1 = (Button) findViewById(R.id.btn_show_fragment1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction ft = manager.beginTransaction();
+                Fragmet1 fragmet1 = new Fragmet1();
+                ft.add(R.id.fragment_container,fragmet1);
+                ft.commit();
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.btn_show_fragment2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction ft = manager.beginTransaction();
+                Fragmet2 fragmet2 = new Fragmet2();
+                ft.add(R.id.fragment_container,fragmet2);
+                ft.commit();
+            }
+        });
     }
 }
