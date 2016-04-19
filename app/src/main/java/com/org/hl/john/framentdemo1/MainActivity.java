eacfd9467ab6b7b97440553601c9bae4cd869d8e
John <475934874@qq.com>
2016-04-19 11:20:17 +0800

    编辑主类目

---
+++ app/src/main/java/com/org/hl/john/framentdemo1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.org.hl.john.framentdemo1;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by John on 2016/4/19.
+ */
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
